69be10ba0091c2019b885d09879d287687136700
Yaourt <yaourt@yaourtprod.net>
2011-07-08 00:53:56 +0200

    WIP

--- src/test/java/net/yaourtprod/sidej/commands/CheckAvailableRedisCommandsIT.java
+++ src/test/java/net/yaourtprod/sidej/commands/CheckAvailableRedisCommandsIT.java
@@ -20,10 +20,17 @@ import net.yaourtprod.sidej.RedisCommand;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class CheckAvailableRedisCommandsIT {
+    
+    /** The logger to be used in this class & children*/
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger("GENERATOR");
+
     /** URL where we can find up-to-date Redis commands list. */
     public static final String COMMANDS_URL_LIST = "https://raw.github.com/antirez/redis-doc/master/commands.json";
     public static final String DOCUMENTATION_BASE_URL = "https://raw.github.com/antirez/redis-doc/master/commands/";
@@ -82,29 +89,39 @@ public class CheckAvailableRedisCommandsIT {
 
     private void generateIRedisClient() throws IOException {
         final StringBuilder strb = new StringBuilder();
-        strb.append("package net.yaourtprod.sidej;\n").append(
-                "public interface IRedisClient<K,V> {\n");
+        strb
+            .append("package net.yaourtprod.sidej;\n\n")
+            .append("public interface IRedisClient<K,V> {\n")
+            .append("\tpublic static enum LINSERT_POSITION {\n")
+            .append("\t\tBEFORE,\n")
+            .append("\t\tAFTER\n")
+            .append("\t}");
         for (final RedisJSONCommand cmd : commands) {
-            strb.append("\t/**\n").append("\t * ").append(cmd.getSummary())
-                    .append(".<br />\n").append("\t * (")
-                    .append(cmd.getGroup()).append(" operation)<br /><br />\n");
-            for (final String doc : getDocumentation(cmd)) {
-                strb.append("\t * ").append(doc).append("\n");
+            if("sort".equals(cmd)){
+                generate_sort(strb);
+                continue;
             }
-            strb.append("\t */\n").append("\tUpdateMe ").append(cmd.getName())
-                    .append('(');
+            if("hmset".equals(cmd)){
+                generate_hmset(strb);
+                continue;
+            }
+            if("linsert".equals(cmd)){
+                generate_linsert(strb);
+                continue;
+            }
+
+            String returnType = generateMethodJavadoc(strb, cmd);
+            strb.append('\t').append(returnType).append(' ').append(cmd.getName()).append('(');
             if (null != cmd.getArguments()) {
                 for (final RedisJSONCommandArgument arg : cmd.getArguments()) {
                     final Object argname = arg.get("name");
                     final Object argtype = arg.get("type");
                     if ("key".equals(argtype)) {
                         strb.append("final K ").append(argname).append(", ");
-                    }
-                    if ("posix time".equals(argtype)) {
+                    } else if ("posix time".equals(argtype)) {
                         strb.append("final long ").append(argname).append(", ");
                     } else {
-                        strb.append("final ").append(argtype).append(' ')
-                                .append(argname).append(", ");
+                        strb.append("final ").append(argtype).append(' ').append(argname).append(", ");
                     }
                 }
                 strb.setLength(strb.length() - 2);
@@ -113,8 +130,43 @@ public class CheckAvailableRedisCommandsIT {
         }
         strb.append("}\n");
 
-        System.out.println(strb.toString());
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info(strb.toString());
+        }
 
+    }
+    
+    private String generateMethodJavadoc(final StringBuilder strb, final RedisJSONCommand cmd) throws IOException {
+        String returnType = "UpdateMe";
+        strb.append("\t/**\n").append("\t * ").append(cmd.getSummary()).append(".<br />\n")
+            .append("\t * (").append(cmd.getGroup()).append(" operation)<br /><br />\n");
+        for (final String doc : getDocumentation(cmd)) {
+            // Skip some specific lines
+            if(doc.startsWith("@description") || doc.startsWith("@return") || doc.startsWith("    @cli")) {
+                // skip
+            } else if(doc.startsWith("@examples")) {
+                strb.append("\t * ").append("Examples :").append("\n");
+            } else if(doc.startsWith("@complexity")) {
+                strb.append("\t * ").append("Complexity :").append("\n");
+            } else if(doc.startsWith("@bulk-reply")) {
+                strb.append("\t * @return a bulk reply ").append(doc.substring("@bulk-reply".length())).append('\n');
+                returnType = "byte[]";
+            } else if(doc.startsWith("@multi-bulk-reply")) {
+                strb.append("\t * @return a multi-bulk reply ").append(doc.substring("@multi-bulk-reply".length())).append('\n');
+                returnType = "List<byte[]>";
+            } else if(doc.startsWith("@status-reply")) {
+                strb.append("\t * @return a status reply ").append(doc.substring("@status-reply".length())).append('\n');
+                returnType = "Status";
+            } else if(doc.startsWith("@integer-reply")) {
+                strb.append("\t * @return an integer reply ").append(doc.substring("@integer-reply".length())).append('\n');
+                returnType = "long";
+            } else {
+                strb.append("\t * ").append(doc).append("\n");
+            }
+        }
+        strb.append("\t */\n");
+        
+        return returnType;
     }
 
     private List<String> getDocumentation(final RedisJSONCommand command)
@@ -129,12 +181,6 @@ public class CheckAvailableRedisCommandsIT {
             reader = new BufferedReader(new InputStreamReader(is));
             String line = null;
             while (null != (line = reader.readLine())) {
-                if (line.startsWith("@")) {
-                    line = line.substring(1);
-                }
-                if (line.startsWith("    @cli")) {
-                    line = line.substring(8);
-                }
                 result.add(line);
             }
         } finally {
@@ -145,4 +191,14 @@ public class CheckAvailableRedisCommandsIT {
         return result;
     }
 
+    private void generate_sort(final StringBuilder strb) {
+        strb.append("\t List<byte[]> sort(final K key, final SortCommand... sortCommands);\n");
+    }
+    private void generate_hmset(final StringBuilder strb) {
+        strb.append("\t Status hmset(final K key, final Map<HK, V> hash);\n");
+    }
+    private void generate_linsert(final StringBuilder strb) {
+        strb.append("\t long linsert(final K key, final LINSERT_POSITION where, final V pivot, final V value);\n");
+    }
+    //long linsert(final K key, final enum where, final string pivot, final string value);
 }
